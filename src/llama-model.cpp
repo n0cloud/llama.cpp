79b5d4e97a5dab932548bae90b10da67b82890dc
Gabriel P. <gabriel.pt@planethoster.info>
2026-07-23 11:19:49 -0400

    TP: allows the usage of 4-10 gpus for stepfun and laguna

--- src/llama-model.cpp
+++ src/llama-model.cpp
@@ -641,6 +641,14 @@ struct ggml_backend_meta_split_state llama_meta_device_get_split_state(const str
             return {blck_size_perf};
         }
 
+        if (ud->model->arch == LLM_ARCH_STEP35 || ud->model->arch == LLM_ARCH_LAGUNA) {
+            GGML_ASSERT(segments.size() == 1);
+            const int64_t max_el      = segments[0].first;
+            if (std::regex_match(tensor_name, pattern_attn_gate_weight)) {
+                return {hparams.n_head(il) / hparams.n_head_kv(il)};
+            }
+        }
+
         // everything else
         GGML_ASSERT(segments.size() == 1);
         return {1};
